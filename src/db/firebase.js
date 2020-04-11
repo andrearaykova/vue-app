e141d6280cc9b71563aca9ec1e7e9116c1a9fb05
andrearaykova <a.p.raykova@gmail.com>
2020-04-11 13:20:59 +0300

    add some code

---
+++ src/db/firebase.js
@@ -0,0 +1,18 @@
+import * as Firebase from "firebase";
+import "firebase/firestore";
+require("firebase/auth");
+
+export const config = {
+  apiKey: "AIzaSyBgvBRq1k7W-BO4otglv77sl9YSt0XUCKg",
+  authDomain: "vue-app-162e6.firebaseapp.com",
+  databaseURL: "https://vue-app-162e6.firebaseio.com",
+  projectId: "vue-app-162e6",
+  storageBucket: "vue-app-162e6.appspot.com",
+  messagingSenderId: "541098740344",
+  appId: "1:541098740344:web:d25502e1e37c99e18b3b8d",
+};
+
+Firebase.initializeApp(config);
+export const auth = Firebase.auth();
+
+export default Firebase.firestore();
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div id="list">
+      <div class="contact-list-item"></div>
+      <div class="contact-email-item"></div>
+      <div class="contact-phone-item"></div>
+    </div>
+  </div>
+</template>
+
+ <script>
+</script>
