751b99bc3274fa93666bc48eee037a22583174eb
andrearaykova <a.p.raykova@gmail.com>
2020-04-11 13:58:48 +0300

    add

--- src/components/AddContact.vue
+++ src/components/AddContact.vue
@@ -1,16 +1,50 @@
 <template>
   <div>
     <a href="#" id="saveBtn" v-on:click="save" />
+    <div class="add-form">
+      <UserInput label="name" v-model="contact.name"></UserInput>
+      <UserInput label="e-mail" mode="texthalf" v-model="contact.email"></UserInput>
+      <UserInput label="phone" mode="texthalf" v-model="contact.phone"></UserInput>
+      <UserInput label="address" mode="textarea" v-model="contact.address"></UserInput>
+      <UserInput platform="fb" mode="social" v-model="contact.fbHandle"></UserInput>
+      <UserInput platform="insta" mode="social" v-model="contact.instaHandle"></UserInput>
+    </div>
   </div>
 </template>
 
  <script>
+import UserInput from "./UserInput";
+
 export default {
+  name: "AddContact",
+  data() {
+    return {
+      contact: {
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+        fbHandle: "",
+        instaHandle: ""
+      }
+    };
+  },
   methods: {
+    clearOut() {
+      for (let item in this.contact) {
+        this.contact[item] = "";
+      }
+    },
     save(e) {
       e.preventDefault();
       alert("saved");
+      alert(this.contact.name + " " + this.contact.email);
+      this.clearOut();
+      this.$router.push("/");
     }
+  },
+  components: {
+    UserInput
   }
 };
 </script> 
---
+++ src/components/UserInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="row-half" v-if="mode === 'texthalf'">
+      <input
+        type="text"
+        class="InputBoxHalf"
+        :placeholder="label"
+        :value="value"
+        @change="returnValue($event.target.value)"
+      />
+    </div>
+    <div class="row-full" v-else-if="mode==='textarea'">
+      <textarea
+        type="text"
+        class="InputBoxFull"
+        :placeholder="label"
+        :value="value"
+        @change="returnValue($event.target.value)"
+      ></textarea>
+    </div>
+
+    <div class="row-half" v-else-if="mode==='social'">
+      <input
+        type="text"
+        class="InputBoxHalf"
+        :value="value"
+        @change="returnValue($event.target.value)"
+      />
+      <span class="icon fbico" v-if="platform==='fb'"></span>
+      <span class="icon instaico" v-if="platform==='insta'"></span>
+    </div>
+    <div class="row-full" v-else>
+      <input
+        type="text"
+        class="InputBoxFull"
+        :placeholder="label"
+        :value="value"
+        @change="returnValue($event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+
+ <script>
+export default {
+  name: "UserInput",
+  props: ["value", "mode", "label", "platform"],
+
+  methods: {
+    returnValue(val) {
+      this.$emit("input", val);
+    }
+  }
+};
+</script> 
